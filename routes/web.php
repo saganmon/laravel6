750ee3d51dd5d8ebf08cc6522fa9cc34363e0a82
saganmon <fevers24@gmail.com>
2020-03-23 18:11:58 +0900

    Route Wildcards

--- routes/web.php
+++ routes/web.php
@@ -13,8 +13,15 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('test', [
-        'name' => request('name')
+Route::get('/posts/{post}', function ($post) {
+    $posts = [
+        'my-first-post' => 'Hello, this is my first blog post!',
+        'my-second-post' => 'Now I am getting the hang of this blogging thing.'
+    ];
+    if(! array_key_exists($post, $posts)) { // エラーハンドリング
+        abort(404, 'Sorry, that post was not found.');
+    }
+    return view('post', [
+        'post' => $posts[$post]
     ]);
 });
